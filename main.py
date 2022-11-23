325bff3fe3e7e4bbee63897b35a4dd5959ec327c
Dmytro Skrynnyk <dmytroskrynnyk@MacBook-Pro-Dmytro.local>
2022-11-23 12:05:58 +0200

    ex_1.4

---
+++ main.py
@@ -0,0 +1,12 @@
+# find sum of elements in list
+total = 0
+
+# creating a list
+list1 = [13, 22, 13]
+list2 = [26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+# using sum() function
+total = sum(list1 + list2)
+
+# printing total value
+print("Sum of all elements in given list: ", total)
